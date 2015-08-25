b67b21e90c62e843fa3d9d30ea0ff7a35a54841d
chava <chava@chavas-Mac.local>
2015-08-24 20:23:14 -0700

    Initial commit

--- weatherWear/Declarations.h
+++ weatherWear/Declarations.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  Declarations.h
 //  weatherWear
 //
 //  Created by chava on 8/24/15.
 //  Copyright (c) 2015 fuentess. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
-
+@interface Declarations : NSObject
 
 @end
-
---
+++ weatherWear/Declarations.m
@@ -0,0 +1,13 @@
+//
+//  Declarations.m
+//  weatherWear
+//
+//  Created by chava on 8/24/15.
+//  Copyright (c) 2015 fuentess. All rights reserved.
+//
+
+#import "Declarations.h"
+
+@implementation Declarations
+
+@end
