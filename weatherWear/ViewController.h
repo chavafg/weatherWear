aa98ebd6a1da463cf8445ebec4a6c849ec60adce
chava <chava@chavas-Mac.local>
2015-08-24 20:14:58 -0700

    Initial Commit

---
+++ weatherWear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weatherWear
+//
+//  Created by chava on 8/24/15.
+//  Copyright (c) 2015 fuentess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
